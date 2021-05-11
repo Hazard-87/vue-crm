ebf09d5e57503a2d2c3bece3856b2af1b17fed92
Hazard <dpetryshkin@gmail.com>
2021-05-11 21:47:09 +0300

    Реализована верстка и переход по страницам

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-main-layout">
+    <Navbar />
+    <SideNav />
+
+    <main class="app-content">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SideNav from "../components/SideNav";
+import Navbar from "../components/Navbar";
+
+export default {
+  name: "MainLayout",
+  components: {
+    SideNav,
+    Navbar,
+  },
+};
+</script>
